a29b116ce10de47c5214e7490df095fcc77e7b53
Capitan Latinamerica <capitan@latinoamerica512.ru>
2026-03-12 12:04:43 +0300

    EC-002: Добавил анимацию перехода на главный экран

---
+++ app/src/main/java/com/supersonic/evercard/features/root/ui/RootActivity.kt
@@ -0,0 +1,12 @@
+package com.supersonic.evercard.features.root.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.supersonic.evercard.R
+
+class RootActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_root)
+    }
+}
